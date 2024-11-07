89590990d37f40025fe5b0bb393bd801e0318840
Parv <parv@Parvs-MacBook-Air.local>
2024-11-07 18:20:23 +0530

    Starting

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1 align="center">Hi 👋, I'm Parv gugnani</h1>
+<h3 align="center">Full Stack Developer</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=parv-gugnani&label=Profile%20views&color=0e75b6&style=flat" alt="parv-gugnani" /> </p>
+
+- 🌱 I’m currently working on Full Stack apps with **Next Js, Node Js**
+
+- 👯 I’m looking to collaborate on **Full Stack Apps**
+
+- 🤝 I’m looking for help with **Node js**
+
+- 💬 Ask me about **C, React, JavaScript**
+
+- 📫 How to reach me **parv18788@gmail.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+  <a href="https://twitter.com/parv_developer" target="_blank">
+    <img align="center" src="https://img.icons8.com/color/48/000000/twitter.png" alt="parv_developer" height="30" width="40" />
+  </a>
+  <a href="https://linkedin.com/in/parv-gugnani/" target="_blank">
+    <img align="center" src="https://img.icons8.com/color/48/000000/linkedin.png" alt="https://www.linkedin.com/in/parv-gugnani/" height="30" width="40" />
+  </a>
+  <a href="https://stackoverflow.com/users/19663011/parv" target="_blank">
+    <img align="center" src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="https://stackoverflow.com/users/19663011/parv" height="30" width="40" />
+  </a>
+  <a href="https://medium.com/@parv9424" target="_blank">
+    <img align="center" src="https://img.icons8.com/color/48/000000/medium-logo.png" alt="@parv9424" height="30" width="40" />
+  </a>
+  <a href="https://www.youtube.com/c/parv-gugnani" target="_blank">
+    <img align="center" src="https://img.icons8.com/color/48/000000/youtube-play.png" alt="https://www.youtube.com/c/parv-gugnani" height="30" width="40" />
+  </a>
+  <a href="https://www.hackerrank.com/parvdev" target="_blank">
+    <img align="center" src="https://img.icons8.com/color/48/000000/hackerrank.png" alt="https://www.hackerrank.com/parvdev" height="30" width="40" />
+  </a>
+</p>
+
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.blender.org/" target="_blank" rel="noreferrer"> <img src="https://download.blender.org/branding/community/blender_community_badge_white.svg" alt="blender" width="40" height="40"/> </a> <a href="https://bulma.io/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/gilbarbara/logos/804dc257b59e144eaca5bc6ffd16949752c6f789/logos/bulma.svg" alt="bulma" width="40" height="40"/> </a> <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://dotnet.microsoft.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg" alt="dotnet" width="40" height="40"/> </a> <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a> <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="40" height="40"/> </a> <a href="https://firebase.google.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40"/> </a> <a href="https://www.framer.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/framer/framer-icon.svg" alt="framer" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://www.adobe.com/in/products/illustrator.html" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/adobe_illustrator/adobe_illustrator-icon.svg" alt="illustrator" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="40" height="40"/> </a> <a href="https://www.php.net" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/> </a> <a href="https://postman.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://redux.js.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40"/> </a> <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="40" height="40"/> </a> <a href="https://unrealengine.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/kenangundogan/fontisto/036b7eca71aab1bef8e6a0518f7329f13ed62f6b/icons/svg/brand/unreal-engine.svg" alt="unreal" width="40" height="40"/> </a> <a href="https://www.adobe.com/products/xd.html" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/adobe-xd.svg" alt="xd" width="40" height="40"/> </a> </p>
+
+<p><img align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=parv-gugnani&show_icons=true&locale=en&layout=compact" alt="parv-gugnani" /></p>
---
+++ src/app/resources/content.js
@@ -0,0 +1,251 @@
+import { InlineCode } from "@/once-ui/components";
+
+const person = {
+    firstName: 'Parv',
+    lastName:  'Gugnani',
+    get name() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    role:      'Full Stack Developer',
+    avatar:    '/images/avatar.jpg',
+    location:  'Asia/India', 
+    languages: ['Hindi', 'English']
+}
+
+const newsletter = {
+    display: true,
+    title: <>Subscribe to {person.firstName}'s Newsletter</>,
+    description: <>Join my newsletter for insights on Web Development, creative engineering, and the latest in frameworks like Next.js and Node.js!</>
+}
+
+const social = [
+    {
+        name: 'GitHub',
+        icon: 'github',
+        link: 'https://github.com/Parv-gugnani',
+    },
+    {
+        name: 'LinkedIn',
+        icon: 'linkedin',
+        link: 'https://www.linkedin.com/in/parv-gugnani/',
+    },
+    {
+        name: 'X',
+        icon: 'x',
+        link: 'https://x.com/parv_developer',
+    },
+    {
+        name: 'Email',
+        icon: 'email',
+        link: 'mailto:parv18788@gmail.com',
+    },
+]
+
+const home = {
+    label: 'Home',
+    title: `${person.name}`,
+    description: `Portfolio website showcasing my work as a ${person.role}`,
+    headline: <>Full Stack Developer</>,
+    subline: <>I'm Parv, a Full Stack Developer with a passion for <InlineCode>Next.js</InlineCode> and backend technologies.<br/> I love building impactful projects and experimenting with new tech stacks 🚀.</>
+}
+
+const about = {
+    label: 'About',
+    title: 'About me',
+    description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+    tableOfContent: {
+        display: true,
+        subItems: true
+    },
+    avatar: {
+        display: true
+    },
+    calendar: {
+        display: true,
+        link: 'https://cal.com/parv7/'
+    },
+    intro: {
+        display: true,
+        title: 'Introduction',
+        description: <>Selene is a Jakarta-based design engineer with a passion for transforming complex challenges into simple, elegant design solutions. Her work spans digital interfaces, interactive experiences, and the convergence of design and technology.</>
+    },
+    work: {
+        display: true,
+        title: 'Work Experience',
+        experiences: [
+            {
+                company: 'MillionNeuron',
+                timeframe: 'April 2022 -  Jun 2022',
+                role: 'Game Developer Intern',
+                achievements: [
+                    <>Collaborated with the development team to create an educational game using Blueprints and C++, enhancing students' learning experience through practical application.</>,
+                    <>Contributed to the development of showcase demos for investors, demonstrating the game's potential and value proposition.</>
+                ],
+                images: [ // optional: leave the array empty if you don't want to display images
+                    {
+                        src: '/images/projects/project-01/cover-01.jpg',
+                        alt: 'Once UI Project',
+                        width: 16,
+                        height: 9
+                    }
+                ]
+            },
+            {
+                company: 'Creativ3',
+                timeframe: '2018 - 2022',
+                role: 'Lead Designer',
+                achievements: [
+                    <>Developed a design system that unified the brand across multiple platforms, improving design consistency by 40%.</>,
+                    <>Led a cross-functional team to launch a new product line, contributing to a 15% increase in overall company revenue.</>
+                ],
+                images: [ ]
+            }
+        ]
+    },
+    studies: {
+        display: true, // set to false to hide this section
+        title: 'Studies',
+        institutions: [
+            {
+                name: 'University of Jakarta',
+                description: <>Studied software engineering.</>,
+            },
+            {
+                name: 'Build the Future',
+                description: <>Studied online marketing and personal branding.</>,
+            }
+        ]
+    },
+    technical: {
+        display: true, // set to false to hide this section
+        title: 'Technical skills',
+        skills: [
+            {
+                title: 'Figma',
+                description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+                images: [
+                    {
+                        src: '/images/projects/project-01/cover-02.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                    {
+                        src: '/images/projects/project-01/cover-03.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                ]
+            },
+            {
+                title: 'Next.js',
+                description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+                images: [
+                    {
+                        src: '/images/projects/project-01/cover-04.jpg',
+                        alt: 'Project image',
+                        width: 16,
+                        height: 9
+                    },
+                ]
+            }
+        ]
+    }
+}
+
+const blog = {
+    label: 'Blog',
+    title: 'Writing about design and tech...',
+    description: `Read what ${person.name} has been up to recently`
+    // Create new blog posts by adding a new .mdx file to app/blog/posts
+    // All posts will be listed on the /blog route
+}
+
+const work = {
+    label: 'Work',
+    title: 'My projects',
+    description: `Design and dev projects by ${person.name}`
+    // Create new project pages by adding a new .mdx file to app/blog/posts
+    // All projects will be listed on the /home and /work routes
+}
+
+const gallery = {
+    label: 'Gallery',
+    title: 'My photo gallery',
+    description: `A photo collection by ${person.name}`,
+    images: [
+        { 
+            src: '/images/gallery/img-01.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-02.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-03.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-04.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-05.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-06.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-07.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-08.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-09.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-10.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-11.jpg', 
+            alt: 'image',
+            orientation: 'vertical'
+        },
+        { 
+            src: '/images/gallery/img-12.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-13.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+        { 
+            src: '/images/gallery/img-14.jpg', 
+            alt: 'image',
+            orientation: 'horizontal'
+        },
+    ]
+}
+
+export { person, social, newsletter, home, about, blog, work, gallery };
